000f051874c558fc8ac773686de73bc42a30be87
Patrick McKeown <pmaccamp@sbcglobal.net>
2018-03-11 21:26:49 -0700

    Create prices.class.php

---
+++ include/classes/prices.class.php
@@ -0,0 +1,31 @@
+<?php
+$defflip = (!cfip()) ? exit(header('HTTP/1.1 401 Unauthorized')) : 1;
+
+class Block extends Base {
+  protected $table = 'prices';
+
+  /**
+   * Specific method to get latest price
+   * @param none
+   * @return last price as double
+   **/
+  public function getLastPrice() {
+    $stmt = $this->mysqli->prepare("SELECT * FROM $this->table ORDER BY height DESC LIMIT 1");
+    if ($this->checkStmt($stmt) && $stmt->execute() && $result = $stmt->get_result()) {
+      $aData = $result->fetch_assoc();
+      if(isset($aData) && isset($aData["price"]){
+        return $aData["price"];
+      } else {
+        return 0.0;
+      }
+    }
+    return $this->sqlError();
+  }  
+}
+
+// Automatically load our class for furhter usage
+$prices = new Prices();
+$prices->setDebug($debug);
+$prices->setMysql($mysqli);
+$prices->setConfig($config);
+$prices->setErrorCodes($aErrorCodes);
