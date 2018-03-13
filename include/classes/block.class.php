a396951f4b14cf23f257e167ce5842b4fd4b0180
Patrick McKeown <pmaccamp@sbcglobal.net>
2018-03-13 00:21:27 -0700

    Update block.class.php

--- include/classes/block.class.php
+++ include/classes/block.class.php
@@ -179,8 +179,8 @@ class Block extends Base {
    * @return bool
    **/
   public function addBlock($block) {
-    $stmt = $this->mysqli->prepare("INSERT INTO $this->table (height, blockhash, confirmations, amount, difficulty, time) VALUES (?, ?, ?, ?, ?, ?)");
-    if ($this->checkStmt($stmt) && $stmt->bind_param('isiddi', $block['height'], $block['blockhash'], $block['confirmations'], $block['amount'], $block['difficulty'], $block['time']) && $stmt->execute())
+    $stmt = $this->mysqli->prepare("INSERT INTO $this->table (height, blockhash, confirmations, amount, difficulty, time, price) VALUES (?, ?, ?, ?, ?, ?)");
+    if ($this->checkStmt($stmt) && $stmt->bind_param('isiddi', $block['height'], $block['blockhash'], $block['confirmations'], $block['amount'], $block['difficulty'], $block['time'], $prices->getLastPrice()) && $stmt->execute())
       return true;
     return $this->sqlError();
   }
